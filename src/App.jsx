ebe32cdcaf0affff0d8c8722f7966197d3b06e6f
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2025-02-28 13:02:21 -0300

    Primer commit del nuevo proyecto basado en la plantilla

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl bg-red-500 font-bold text-white p-4">
+      PLANTILLA REACT VITE + TAILWINDCSS
+    </h1>
+  )
+}
+
+export default App
